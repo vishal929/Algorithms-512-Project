1d5dc18587aab38c8a44b4d0cd1cc9b815c217f0
vishal929 <48373882+vishal929@users.noreply.github.com>
2021-12-12 13:56:16 -0500

    Need to implement partition and median finding for kd tree

--- dataStructures.py
+++ dataStructures.py
@@ -1,4 +1,14 @@
-# python file that houses our defined data structures
+# python file that houses our defined data structures and some helper functions
+
+# helper function to get the median of the dataset based on a specific feature
+def medianFeature(dataArray,featureToUse):
+
+
+# helper function to pivot a dataset based on a certain feature value
+# pivots the array about the median of the array
+def partitionFeature(dataset, i,j, featureToPivot, indexToPivot):
+    lower = []
+    higher = []
 
 class maxHeap(object):
     def __init__(self):
@@ -18,8 +28,7 @@ class maxHeap(object):
         # getting the parent, doing a comparison
         # if this element is greater than the parent, we have to swap and repeat the sift
         # using an array for recursion based on stack, instead of recursive calls
-        siftStack = []
-        siftStack.append(index)
+        siftStack = [index]
         while len(siftStack)>0:
             index = siftStack.pop()
             if index == 0:
@@ -38,8 +47,7 @@ class maxHeap(object):
         # we get the children of the current node and perform a comparison
         # we will use a stack for recursion instead of recursive calls
         # note that if the current element is a leaf, then there is no need to sift down any further
-        siftStack = []
-        siftStack.append(index)
+        siftStack = [index]
         while len(siftStack)>0:
             siftDownIndex = siftStack.pop()
             leftChild = (2*siftDownIndex)+1
@@ -76,15 +84,62 @@ class maxHeap(object):
         self.arr.pop()
         self.siftDown(0)
 
+# class for nodes in a kdtree
+class kdNode(object):
+    def __init__(self,data,splitAxis,left,right):
+        # data is a tuple of features for each datapoint
+        self.data = data
+        # split axis is the feature on which this node was split
+        self.splitAxis = splitAxis
+        # node has a left and right child for the future splits
+        self.left = left
+        self.right = right
 
 # class for kd trees datastructure
 class kdTree(object):
-    def __init__(self):
-        # do stuff
-        stuff =1
-    def constructTree(self,data):
-        # constructing tree from dataset
-        stuff =1
+    # kdTree is defined by a root node
+    # construction is defined by some dataset
+    def __init__(self,data):
+        self.dataset = data
+        # getting the number of features in the dataset based on the length of a data tuple
+        self.numFeatures = len(data[0])
+        # constructing the kdtree from data and setting the root node
+        self.rootNode = self.constructTreeDriver()
+    # recursive construction of a kdtree from a dataset
+    # data is an array of element tuples where parts of the tuple are features
+    # i and j are indices for recursion
+    # featureToSplit is just the feature number to split the next level on (dimension)
+    def constructTree(self,i,j,featureToSplit):
+        # recursively constructing tree from the dataset for the kdtree
+        # resetting the feature split to "wrap around"
+        if featureToSplit > self.numFeatures:
+            featureToSplit=0
+
+        if i == j:
+            # then we only have 1 piece of data in this subtree
+            return kdNode(self.dataset[i], featureToSplit, None, None)
+
+        if j<i:
+            # no more data
+            return None
+
+        # pivoting data based on which feature we are using now
+        mid = pivotFeature(self.dataset,i,j,featureToSplit)
+        # incrementing the feature to split on next
+        featureToSplit += 1
+        parent = kdNode(self.dataset[mid],featureToSplit, None, None)
+        parent.left = self.constructTree(i,mid-1,featureToSplit)
+        parent.right = self.constructTree(mid+1,j,featureToSplit)
+
+        # returning the parent
+        return parent
+
+    # driver to construct a tree recursively and set it as the root node
+    # this is called during init with a dataset of tuples
+    def constructTreeDriver(self):
+        self.rootNode = self.constructTree(0,len(self.dataset)-1,0)
+
+
 
 # class for the multilayer graph to be implemented as part of the hierarchal small world graphs implementation
 class multiLayerGraph(object):
