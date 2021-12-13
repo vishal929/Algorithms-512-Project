6450efefe4dad181ed697281b7512273552bf452
vishal929 <48373882+vishal929@users.noreply.github.com>
2021-12-12 22:36:17 -0500

    Grabbing iris works

--- dataGrabber.py
+++ dataGrabber.py
@@ -1,5 +1,11 @@
 # file houses logic for grabbing necessary data, and cleaning/transforming it for use with k-nn
+import pandas as pd
 
 # grabbing iris dataset
+def grabIris():
+    data = pd.read_csv('iris.data',names=['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'class'], sep=',')
+    return data
+
+
 
 # grabbing imdb dataset
